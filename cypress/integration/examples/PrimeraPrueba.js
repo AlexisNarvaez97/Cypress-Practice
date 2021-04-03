020b61c5bea803d268de6b0ba17a3a472d89539e
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2021-04-02 20:20:14 -0600

    Segundo caso de prueba

--- cypress/integration/examples/PrimeraPrueba.js
+++ cypress/integration/examples/PrimeraPrueba.js
@@ -3,11 +3,12 @@
 
 describe('Primer conjunto de casos de prueba', () => {
 
+    beforeEach(() => {
+        cy.visit('http://automationpractice.com/index.php')
+    })
+
     // Caso de prueba
     it('Contabilizar la cantidad de elementos en la sección de página principal', () => {
-
-        cy.visit('http://automationpractice.com/index.php')
-
         // Verificar el tamaño de elementos
         cy.get('#homefeatured .product-container').should('have.length', 7)
 
@@ -17,6 +18,22 @@ describe('Primer conjunto de casos de prueba', () => {
 
         cy.get('@productosPopulares').should('have.length', 7)
 
+    });
+
+
+    it('Agregar elementos de tipo "Blouse" al carrito de compra desde la página principal', () => {
+        cy.get('#homefeatured .product-container').as('productosPopulares')
+        cy.get('@productosPopulares')
+        .find('.product-name')
+        .each(($el, idx, $list) => {
+            if ($el.attr('title') === 'Blouse') {
+                cy.log('Se ha encontrado el elemento buscado')
+                // cy.get('@productosPopulares').eq(idx).find('.button.ajax_add_to_cart_button span').click();
+                cy.get('@productosPopulares').eq(idx).contains('Add to cart').click();
+            }
+
+        })
+        
     });
     
 });
